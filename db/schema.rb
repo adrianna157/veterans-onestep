427230141602376b8b109d5e4b8d8f18380dc140
Adrianna Guevarra <adriannaestelleguevarra@gmail.com>
2024-03-16 20:31:05 -0700

    Create Users table and User model
    In this commit, a migration to create the Users table in the database has been added with necessary fields including name, email, username, password_digest, admin status, and slug. Also, a basic User model has been generated alongside model tests and fixtures for future testing. Some new gems have also been included in the application's configuration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_18_175840) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_01_233155) do
   create_table "active_storage_attachments", charset: "utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -56,6 +56,17 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_175840) do
     t.string "name"
     t.string "email"
     t.string "phone_number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "username"
+    t.boolean "admin"
+    t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
